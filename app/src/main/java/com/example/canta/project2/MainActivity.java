5cf63356bab869d327a5d47c8a60196c28363c29
Ahmet Can Turgut <aturgut@ku.edu.tr>
2017-05-10 01:22:55 +0300

    login added

--- app/src/main/java/com/example/canta/project2/MainActivity.java
+++ app/src/main/java/com/example/canta/project2/MainActivity.java
@@ -1,50 +1,95 @@
 package com.example.canta.project2;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     Boolean userNameFlag = true;
+
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+
+    private EditText emailText;
+    private EditText passwordText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        mAuth = FirebaseAuth.getInstance();
 
-        final EditText playerNameEditText = (EditText) findViewById(R.id.player_name_edittext);
-        Button goButton = (Button) findViewById(R.id.go_button);
 
-        playerNameEditText.setText(Player.getPlayerName());
 
-        goButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+        emailText = (EditText) findViewById(R.id.player_name_edittext);
+        passwordText = (EditText) findViewById(R.id.password_edit_text);
+        Button logIn = (Button) findViewById(R.id.go_button);
+        Button signIn = (Button) findViewById(R.id.sign_in_button);
 
-                if (!playerNameEditText.getText().toString().equals("") && !playerNameEditText.getText().toString().equals("Enter Your Name")) {
 
-                    Player.getInstance().setPlayerName(playerNameEditText.getText().toString());
-                    Intent intent = new Intent(getApplicationContext(), SecondActivity.class);
-                    startActivity(intent);
+        logIn.setOnClickListener(this);
+    }
 
+    @Override
+    public void onClick(View v) {
+
+        switch (v.getId()) {
+            case  R.id.go_button: {
+
+                if (!emailText.getText().toString().equals("") && !emailText.getText().toString().equals("Enter Your Name")) {
+
+                    mAuth.signInWithEmailAndPassword(emailText.getText().toString(), passwordText.getText().toString())
+                            .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                                @Override
+                                public void onComplete(@NonNull Task<AuthResult> task) {
+                                    if (task.isSuccessful()) {
+                                        // Sign in success, update UI with the signed-in user's information
+
+                                        FirebaseUser user = mAuth.getCurrentUser();
+                                        Player.getInstance().setPlayerName(user.toString());
+
+                                        Intent intent = new Intent(getApplicationContext(), SecondActivity.class);
+                                        startActivity(intent);
+
+
+                                    } else {
+
+                                        Toast.makeText(getApplicationContext(), "Fail To Login!",
+                                                Toast.LENGTH_LONG).show();
+
+                                    }
+
+                                    // ...
+                                }
+                            });
                 }
+                break;
             }
-        });
 
-        playerNameEditText.setOnFocusChangeListener(new View.OnFocusChangeListener() {
-            @Override
-            public void onFocusChange(View view, boolean b) {
 
-                if (userNameFlag) {
+            case R.id.sign_in_button: {
 
-                    playerNameEditText.setText("");
-                    userNameFlag = false;
-                }
+                break;
             }
-        });
+
+
+        }
+
+
+
+
+
     }
 }
